a44ae4b2c43a43a370c620ecde186e71788e6375
Nicholas D'Amico <nickalan82@icloud.com>
2017-09-08 18:59:33 -0400

    updated join table name in app/models/auction and auctioneer

--- app/models/auctioneers.rb
+++ app/models/auctioneers.rb
@@ -1,4 +1,4 @@
 class Auctioneer < ActiveRecord::Base
-	has_many :auction_auctioneers
-	has_many :auctions, :through => :auction_auctioneers
+	has_many :auction_auctioneer
+	has_many :auctions, :through => :auction_auctioneer
 end
