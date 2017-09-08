95424f8be06bbf37ef9639902120814bd7dce860
Nicholas D'Amico <nickalan82@icloud.com>
2017-09-08 11:12:24 -0400

    cleaned up code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,8 +9,42 @@ class UsersController < ApplicationController
 	end
 
 	post '/signup' do
-		binding.pry
+		if User.exists?(email: params["email"])
+			redirect to :'/login'
+		elsif !User.new(params).valid?
+			flash[:message] = "Username/Email already taken"
+			erb :'users/signup.html'
+		end
+		@user = User.create(params)
+		session[:id] = @user.id
+		redirect to :'/auctions'
 	end
+
+	get '/login' do
+		redirect to :'/auctions' if logged_in?		
+		erb :'users/login.html'
+	end
+
+	post '/login' do
+		
+		if @user = authorize_user(params)
+			session[:id] = @user.id
+		else
+			redirect to :'/signup'
+		end
+		redirect to :'/auctions'
+	end
+
+	get '/logout' do
+		if logged_in?
+			session.clear
+			redirect to '/login'
+		else
+			redirect to '/'
+		end
+	end
+
+	
 
 end
 
