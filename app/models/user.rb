80a03654402f2055d5e1d7061e07020cde7aa6f1
Nicholas D'Amico <nickalan82@icloud.com>
2017-09-08 11:11:09 -0400

    Removed validations at the moment for early development causing too many issues ATM, will add back later

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,15 +2,12 @@ class User < ActiveRecord::Base
 	has_many :auctions
  	has_secure_password
 
- 	validates :email,  presence: true,
- 					   length: { minimum: 2 },
- 					   allow_blank: false
+ 	validates :email,  presence: true, 					  
+ 					   uniqueness: true
 
- 	validates :password, presence: true,
- 						 length: { in: 6..20 },
+ 	validates :password, presence: true, 						 
  						 allow_blank: false
 
-	validates :username, presence: true,
-						 length: { minimum: 2 },
+	validates :username, presence: true,						 
 						 allow_blank: false
 end
