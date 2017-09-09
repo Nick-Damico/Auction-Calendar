b57d3771dcccfabf7fcf552e1f67464257192aad
Nicholas D'Amico <nickalan82@icloud.com>
2017-09-09 12:34:25 -0400

    Added Validations for :name, notes for potential new attributes

--- app/models/auctioneers.rb
+++ app/models/auctioneers.rb
@@ -1,4 +1,11 @@
 class Auctioneer < ActiveRecord::Base
 	has_many :auction_auctioneer
 	has_many :auctions, :through => :auction_auctioneer
+
+	validates :name,	presence: true, 					  
+ 						uniqueness: true
+
+ 	# Add Migrations for columns,
+ 		# License Number w/ Validations
+ 		# Designations?
 end
