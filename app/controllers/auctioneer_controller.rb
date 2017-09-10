a4a3dee83b2e5d2af4aec32835a17961e11a3685
Nicholas D'Amico <nickalan82@icloud.com>
2017-09-10 07:56:06 -0400

    Added controller actions to view individual auctioneers, create new auctioneers

--- app/controllers/auctioneer_controller.rb
+++ app/controllers/auctioneer_controller.rb
@@ -8,4 +8,24 @@ class AuctioneerController < ApplicationController
 		erb :'auctioneers/auctioneers.html'
 	end
 
+	get '/auctioneers/new' do
+		redirect to '/' if !logged_in?
+		erb :'auctioneers/new.html'
+	end
+
+	 get '/auctioneers/:id' do
+      @auctioneer = Auctioneer.find(params[:id])
+      erb :'auctioneers/show.html'
+    end
+
+	post '/auctioneers' do
+		if Auctioneer.new(params["auctioneer"]).valid?
+			Auctioneer.create(params["auctioneer"])
+		else
+			# Flash message in future, to notify auctioneer is in DB.
+			redirect to '/auctioneers/new'
+		end
+		erb :'auctioneers/auctioneers.html'
+	end
+
 end
