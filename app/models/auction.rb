0f43376eb7b96c3ff8879298f3a32b18ea8fa85a
Nicholas D'Amico <nickalan82@icloud.com>
2017-09-13 08:47:34 -0400

    Added validations and required attribute to input fields for auctions/new

--- app/models/auction.rb
+++ app/models/auction.rb
@@ -2,4 +2,12 @@ class Auction < ActiveRecord::Base
 	belongs_to :user
 	has_many :auction_auctioneer
 	has_many :auctioneers, :through => :auction_auctioneer, dependent: :destroy
+
+	validates :title,	presence: true, 					  
+ 						uniqueness: true,
+ 						allow_blank: false
+
+ 	validates :description,		presence: true, 					  
+ 								uniqueness: true,
+ 								allow_blank: false
 end
