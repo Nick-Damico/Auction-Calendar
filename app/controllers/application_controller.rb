5c3b83edf16babb0048a351a2f2be8c4e5d7a5ce
Nicholas D'Amico <nickalan82@icloud.com>
2017-09-22 22:44:05 -0400

    Replaced #get_error_msgs with #error_msg its a more abstract and flexible way of looping through an array of class objects to look for invalid instances and return the specific validation errors, it utilizes new helper method #params_to_obj which takes in params from a http request and converts them class instances. Renamed #get_error_msgs to #patch_auction_errors, #patch_auctioneer_errors for checking if class instances are valid before updating model attributes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,43 +10,56 @@ class ApplicationController < Sinatra::Base
 	end
 
 	helpers do
-		def current_user
-	      @current_user ||= User.find_by(id: session[:id])
+
+		def authorize_user(params)
+	    	User.find_by(:username => params[:username]).try(:authenticate, params[:password])
 	    end
 
-	    def logged_in?
-	      !!current_user
+		def current_user
+	     	@current_user ||= User.find_by(id: session[:id])
 	    end
 
 	    def user_exists?(params)
 	    	User.exists?(email: params["email"])
 	    end
 
-	    def authorize_user(params)
-	    	User.find_by(:username => params[:username]).try(:authenticate, params[:password])
+	    def logged_in?
+	      !!current_user
 	    end
 
-	    # Escapes HTML Code if embedded in content submitted.
-	    def h(string)
-  			Rack::Utils.escape_html(string)
+	    def params_to_obj(params)
+	    	params.each_key.map do |key|
+	    		Object.const_get(key.capitalize).new(params[key])
+	    	end
+	    end
+
+		def error_msg(params)
+			error_msgs = []
+	    	params_to_obj(params).each.map do |obj|	
+	    		if obj.invalid? && !obj.class.column_names.include?("name")
+	    			obj.errors.full_messages 
+	    		elsif obj.invalid? && (obj.class.column_names.include?("name") && !obj["name"].empty?)
+	    			obj.errors.full_messages
+	    		end
+	    	end.compact
 		end
 
-		def get_error_msgs(params)
+		def patch_auction_errors(params)
 			error_msgs = []
 			auction = Auction.new(params["auction"])
 			auctioneer = Auctioneer.new(params["auctioneer"])	
-			if auction.invalid? || (auctioneer.invalid? && !params["auctioneer"]["name"].empty?)
-				error_msgs << auction.errors.full_messages	if auction.invalid?
-				error_msgs << auctioneer.errors.full_messages if auctioneer.invalid? 				
+			if auction.invalid? 
+				error_msgs << auction.errors.full_messages
+			elsif auctioneer.invalid? && !params["auctioneer"]["name"].empty?
+				error_msgs << auctioneer.errors.full_messages	
 			end	
-
 			error_msgs
 		end
 
-		def get_auctioneer_error_msgs(params)
+		def patch_auctioneer_errors(params)
 			error_msgs = []
 			a = Auctioneer.new(params["auctioneer"])
-			unless a.valid?
+			if a.invalid? && (a["auctioneer_license"] != params["auctioneer"]["auctioneer_license"])
 				error_msgs = a.errors.full_messages
 			end
 			error_msgs
